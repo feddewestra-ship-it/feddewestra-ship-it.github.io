086aeaa24427d51afaa2ecc4ca0bf38f63e5be4c
nexo studio's <feddewestra@leerling.heemgaard.nl>
2026-03-06 14:09:36 +0100

    Refactor RAM check and remove console checks

--- boot.C
+++ boot.C
@@ -3,18 +3,26 @@
 
 #define MIN_RAM_MB 128
 
+void crash(EFI_SYSTEM_TABLE *SystemTable, CHAR16 *reason)
+{
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"GEFAALD: ");
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, reason);
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"\n");
+
+    while (1)
+    {
+        __asm__("hlt");
+    }
+}
+
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
     InitializeLib(ImageHandle, SystemTable);
 
     Print(L"NexoOS build 000.001\n");
-    Print(L"Hardware controle gestart...\n\n");
+    Print(L"Hardware controle...\n");
 
     EFI_BOOT_SERVICES *BootServices = SystemTable->BootServices;
-
-    /* ============================= */
-    /* RAM CHECK                     */
-    /* ============================= */
 
     UINTN memoryMapSize = 0;
     EFI_MEMORY_DESCRIPTOR *memoryMap = NULL;
@@ -48,59 +56,25 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 
     UINT64 totalRAM = 0;
 
-    for(UINTN i = 0; i < memoryMapSize / descriptorSize; i++)
+    for (UINTN i = 0; i < memoryMapSize / descriptorSize; i++)
     {
-        EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR*)
-            ((UINT8*)memoryMap + (i * descriptorSize));
+        EFI_MEMORY_DESCRIPTOR *desc =
+            (EFI_MEMORY_DESCRIPTOR*)((UINT8*)memoryMap + (i * descriptorSize));
 
         totalRAM += desc->NumberOfPages * 4096;
     }
 
     UINT64 ramMB = totalRAM / (1024 * 1024);
 
-    Print(L"Gevonden RAM: %d MB\n", ramMB);
-
-    if(ramMB < MIN_RAM_MB)
+    if (ramMB < MIN_RAM_MB)
     {
-        Print(L"FOUT: onvoldoende RAM!\n");
-        Print(L"NexoOS kan niet starten.\n");
-
-        while(1)
-        {
-            __asm__("hlt");
-        }
+        crash(SystemTable, L"te weinig RAM");
     }
 
-    Print(L"RAM controle OK\n");
-
-    /* ============================= */
-    /* CONSOLE CHECK                 */
-    /* ============================= */
-
-    if(SystemTable->ConOut == NULL)
-    {
-        while(1)
-        {
-            __asm__("hlt");
-        }
-    }
-
-    Print(L"Console OK\n");
-
-    /* ============================= */
-    /* OPSLAG CHECK (placeholder)    */
-    /* ============================= */
-
-    Print(L"Opslag: verwacht FAT32 EFI boot pad\n");
-
-    /* ============================= */
-    /* RESULTAAT                     */
-    /* ============================= */
-
-    Print(L"\nHardware compatibel.\n");
+    Print(L"RAM OK\n");
     Print(L"NexoOS boot kan doorgaan.\n");
 
-    while(1)
+    while (1)
     {
         __asm__("hlt");
     }
